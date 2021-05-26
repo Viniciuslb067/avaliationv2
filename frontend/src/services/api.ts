d64002d05cf21f3638b60b71a4cbedefd4c80e90
Viniciuslb <viniciuslb067@gmail.com>
2021-05-26 19:10:34 -0300

    feat: interceptors response with axios

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,23 @@
-import axios from "axios";
+import Router from "next/router";
+import axios, { AxiosError } from "axios";
+import { parseCookies } from "nookies";
+
+let cookies = parseCookies();
+let failedRequestQueue = [];
 
 export const api = axios.create({
-  baseURL: "http://10.120.48.53:3001",
+  baseURL: "http://localhost:3001",
+  headers: {
+    Authorization: `Bearer ${cookies["feedback.token"]}`,
+  },
 });
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    if (error.response.status === 301) {
+      Router.push("/");
+    }
+  }
+);
