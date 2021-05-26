f2003f6d5d0772be0c8a55efb79838ca081d5cd0
Viniciuslb <viniciuslb067@gmail.com>
2021-05-26 19:09:28 -0300

    feat: changing status code

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -18,12 +18,11 @@ function generateToken(params = {}) {
 router.get("/check", async (req, res) => {
   const token = req.query.token.split(' ')[1];
   if (!token) {
-    res.json({ status: 401, error: "Token inexistente" });
+    res.json({ status: 301, error: "Token inexistente" });
   } else {
     jwt.verify(token, authConfig.secret, (err, decoded) => {
       if (err) {
-        console.log(err)
-        return res.status(401).json({ status: 2, error: "Token inválido" });
+        return res.status(301).json({ status: 2, error: "Token inválido" });
       } else {
         return res.json({ status: 200 });
       }
