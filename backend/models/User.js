40b979bcfff45de72a806a1f491b8c3eae213d0f
Vinicius Lima Barbosa <viniciuslb067@gmail.com>
2021-06-14 13:15:20 -0300

    refactor: changes on User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require("../database");
-const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -8,24 +7,14 @@ const UserSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    unique: true,
     required: true,
     lowercase: true,
   },
-  password: {
+  cpf: {
     type: String,
+    unique: true,
     required: true,
     select: false,
-  },
-  role: {
-    type: String,
-    required: true,
-    default: "Técnico",
-  },
-  access: {
-    type: String,
-    required: true,
-    default: "Pendente",
   },
   createdAt: {
     type: Date,
@@ -33,12 +22,6 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-UserSchema.pre("save", async function (next) {
-  const hash = await bcrypt.hash(this.password, 10);
-  this.password = hash;
-
-  next();
-});
 
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
