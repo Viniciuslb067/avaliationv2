ad542ec4404375a64d7d365e1b63bc833852a445
Viniciuslb067 <viniciuslb067@gmail.com>
2021-07-06 11:52:35 -0300

    docs: api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,14 +4,5 @@ import axios, { AxiosError } from "axios";
 export const api = axios.create({
   baseURL: "https://10.120.49.181:3001",
 });
-api.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error: AxiosError) => {
-    if (error.response.status === 401) {
-      Router.push("/"); 
-    }
-  }
-);
+
 
