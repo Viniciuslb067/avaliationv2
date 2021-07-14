73ffc93a98b512f1a29d1ce606c1eae8b53d6b6e
Viniciuslb067 <viniciuslb067@gmail.com>
2021-07-14 10:41:44 -0300

    fix: request using next server

---
+++ frontend/src/services/axios.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { parseCookies } from "nookies";
+
+export function getAPIClient(ctx?: any) {
+  const { "feedback.token": token } = parseCookies(ctx);
+
+  const api = axios.create({
+    baseURL: "http://10.120.49.181:3001",
+  });
+
+  if (token) {
+    api.defaults.headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  return api;
+}
