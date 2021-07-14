a7bf0f90688042e56e1a7670ddd1351266ed87e1
Viniciuslb067 <viniciuslb067@gmail.com>
2021-07-14 11:22:57 -0300

    refactor: api to use in browser request

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,21 +1,3 @@
-import axios from "axios";
-import { parseCookies } from "nookies";
+import { getAPIClient } from "./axios";
 
-const { "feedback.token": token } = parseCookies();
-
-export const api = axios.create({
-  baseURL: "http://10.120.49.181:3001",
-});
-
-console.log(`Bearer ${token}`)
-
-axios.defaults.headers.common['Content-Type'] = `Bearer ${token}`;
-
-// api.interceptors.request.use(
-//   (config) => {
-//     console.log(config)
-//     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
+export const api = getAPIClient();
