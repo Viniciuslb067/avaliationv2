0624df4cf1713acec62f16c6726b696e7861b2fd
Viniciuslb067 <viniciuslb067@gmail.com>
2021-07-21 13:56:43 -0300

    refactor: api docs middleware

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const fs = require("fs");
-const https = require("https");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerDocs = require("./swagger.json");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 require("./controllers/authController")(app);
 require("./controllers/assessmentController")(app);
@@ -15,16 +18,6 @@ require("./controllers/systemController")(app);
 require("./controllers/userController")(app);
 require("./controllers/entrieController")(app);
 
-
-// https
-//   .createServer(
-//     {
-//       key: fs.readFileSync("key.pem"),
-//       cert: fs.readFileSync("cert.pem"),
-//     },
-//     app
-//   )
-
-  app.listen(3001, () => {
-    console.log("Servidor iniciado!");
-  });
+app.listen(3001, () => {
+  console.log("Servidor iniciado!");
+});
